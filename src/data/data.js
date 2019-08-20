06e25ff6c788bbcf35c352245798d2301484419a
Raul Mogos <rmogos@RONET1.LOCAL>
2019-08-20 17:50:13 +0300

    recovered lost code

--- src/data/data.js
+++ src/data/data.js
@@ -7,63 +7,6 @@ const generateImage = faker.image.avatar;
 const generateFirstName = faker.name.firstName;
 const generateLastName = faker.name.lastName;
 
-// const data2 = [{
-//   id: generateUniqueID(),
-//   firstName: 'Alin',
-//   lastName: 'Ionescu',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Leona',
-//   lastName: 'Sherman',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Annette',
-//   lastName: 'Carlson',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Ervin',
-//   lastName: 'Watkins',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Philip',
-//   lastName: 'Pope',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Viorel',
-//   lastName: 'Popescu',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// },
-// {
-//   id: generateUniqueID(),
-//   firstName: 'Elon',
-//   lastName: 'Musk',
-//   image: generateImage(),
-//   likes: 0,
-//   isChecked: false
-// }];
-
 const data = [];
 
 for (let i = 0; i < 17; i++) {
