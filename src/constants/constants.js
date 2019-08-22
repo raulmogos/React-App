91a175c0f1034edcdd3f28453b1111d1eb235838
Raul Mogos <rmogos@RONET1.LOCAL>
2019-08-22 16:12:27 +0300

    solved comments

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,3 +9,14 @@ export const CUSTOM_TYPES_BUTTONS = {
   trash: 'icon trash',
   standard: 'ui button'
 };
+
+export const STYLE = {
+  BUTTON: {
+    standard: 'circular black ui icon button',
+    hover: 'circular olive ui icon button'
+  },
+  CHECKBOX: {
+    checked: 'check black circular icon',
+    unchecked: 'circular icon'
+  }
+};
