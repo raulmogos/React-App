a24301258cf06521731a2ca87b80257ed61ad654
Raul Mogos <rmogos@RONET1.LOCAL>
2019-09-26 17:51:12 +0300

    added more tests for components

--- src/components/add-contact-form/add-contact-form.spec.js
+++ src/components/add-contact-form/add-contact-form.spec.js
@@ -1,16 +1,187 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import AddContactForm from './add-contact-form.component';
+import * as helpers from '../../helpers/helper';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const setUp = (props = {}) => shallow(<AddContactForm {...props} />);
 
+const params = {
+  names: {
+    firstName: 'firstName',
+    lastName: 'lastName',
+    imageUrl: 'imageUrl'
+  },
+  values: {
+    firstName: 'Ana',
+    lastName: 'Maria',
+    imageUrl: 'https://stackoverflow.com'
+  },
+  valuesForErrors: {
+    firstName: 'Ana!@#$',
+    imageUrl: 'stackoverflowasd.com'
+  }
+};
+
+const props = {
+  _addContact: jest.fn(),
+  contacts: []
+};
+
 describe('add contact component', () => {
-  test('should render ok', () => {
-    const component = setUp();
-    let form = component.find('.form');
-    expect(form.length).toEqual(1);
-    form = component.find('.fields');
-    expect(form.length).toEqual(2);
+
+  let component;
+  let instance;
+  
+  beforeEach(() => {
+    component = setUp(props);
+    instance = component.instance();
+  });
+
+  describe('onFormInputChange', () => {
+    test('firstName, no error', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.firstName,
+          value: ` ${params.values.firstName} `
+        }
+      });
+      expect(instance.state.firstName).toEqual(params.values.firstName);
+      expect(instance.state.firstNameError).toBeFalsy();
+    });
+    test('firstName, with error', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.firstName,
+          value: ` ${params.valuesForErrors.firstName} `
+        }
+      });
+      expect(instance.state.firstName).toEqual(params.valuesForErrors.firstName);
+      expect(instance.state.firstNameError).toBeTruthy();
+    });
+    test('iamgeUrl, no error', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.imageUrl,
+          value: ` ${params.values.imageUrl} `
+        }
+      });
+      expect(instance.state.imageUrl).toEqual(params.values.imageUrl);
+      expect(instance.state.imageUrlError).toBeFalsy();
+    });
+    test('iamgeUrl, with error', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.imageUrl,
+          value: ` ${params.valuesForErrors.imageUrl} `
+        }
+      });
+      expect(instance.state.imageUrl).toEqual(params.valuesForErrors.imageUrl);
+      expect(instance.state.imageUrlError).toBeTruthy();
+    });
+  });
+
+  describe('isButtonDisabled', () => {
+    test('to be false', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.firstName,
+          value: params.values.firstName
+        }
+      });
+      instance.onFormInputChange({
+        target: {
+          name: params.names.lastName,
+          value: params.values.lastName
+        }
+      });
+      instance.onFormInputChange({
+        target: {
+          name: params.names.imageUrl,
+          value: params.values.imageUrl
+        }
+      });
+      expect(instance.isButtonDisabled()).toBeFalsy();
+    });
+    test('to be true', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.lastName,
+          value: params.values.lastName
+        }
+      });
+      instance.onFormInputChange({
+        target: {
+          name: params.names.imageUrl,
+          value: params.values.imageUrl
+        }
+      });
+      expect(instance.isButtonDisabled()).toBeTruthy();
+    });
+    test('to be true, with error', () => {
+      instance.onFormInputChange({
+        target: {
+          name: params.names.firstName,
+          value: params.valuesForErrors.firstName
+        }
+      });
+      instance.onFormInputChange({
+        target: {
+          name: params.names.lastName,
+          value: params.values.lastName
+        }
+      });
+      instance.onFormInputChange({
+        target: {
+          name: params.names.imageUrl,
+          value: params.values.imageUrl
+        }
+      });
+      expect(instance.isButtonDisabled()).toBeTruthy();
+    });
+  });
+
+  describe('handleSubmit', () => {
+    let event;
+    beforeEach(() => {
+      event = {
+        preventDefault: jest.fn()
+      };
+      instance.openDuplicateContactPopup = jest.fn();
+    });
+    test('if contact is NOT unique', () => {
+      helpers.isContactUnique = jest.fn().mockImplementation(() => false);
+      instance.handleSubmit(event);
+      expect(event.preventDefault.mock.calls.length).toBe(1);
+      expect(instance.openDuplicateContactPopup.mock.calls.length).toBe(1);
+      expect(props._addContact.mock.calls.length).toBe(0);
+    });
+    test('if contact is unique', () => {
+      helpers.isContactUnique = jest.fn().mockImplementation(() => true);
+      instance.handleSubmit(event);
+      expect(event.preventDefault.mock.calls.length).toBe(1);
+      expect(instance.openDuplicateContactPopup.mock.calls.length).toBe(0);
+      expect(props._addContact.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('openDuplicateContactPopup', () => {
+    test('if it behaves correctly', () => {
+      instance.openDuplicateContactPopup();
+      expect(instance.state.showPopup).toBeTruthy();
+      instance.setState = jest.fn();
+      instance.openDuplicateContactPopup();
+      expect(instance.setState.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('closePopUp', () => {
+    test('if it behaves correctly', () => {
+      instance.closePopUp();
+      expect(instance.state.showPopup).toBeFalsy();
+      instance.setState = jest.fn();
+      instance.closePopUp();
+      expect(instance.setState.mock.calls.length).toBe(1);
+    });
   });
 });
---
+++ src/components/contact/contact.spec.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Contact from './contact.component';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const setUp = (props = {}) => shallow(<Contact {...props} />);
+
+const props = {
+  contact: {
+    id: '2389eh239fn2',
+    firstName: 'Maria',
+    lastName: 'Ioana',
+    image: 'https://asdasdas',
+    likes: 3,
+    isChecked: false
+  },
+  isFavourite: false,
+  _deleteContact: jest.fn(),
+  _changeIsChecked: jest.fn(),
+  _increaseLikes: jest.fn(),
+  _decreaseLikes: jest.fn()
+};
+
+describe('contact component', () => {
+  let component;
+  let instance;
+  beforeEach(() => {
+    component = setUp(props);
+    instance = component.instance();
+  });
+  describe('changeIsCheckedStatus', () => {
+    test('if it is called', () => {
+      instance.changeIsCheckedStatus();
+      expect(props._changeIsChecked.mock.calls.length).toBe(1);
+    });
+  });
+  describe('increaseLikes', () => {
+    test('if it is called', () => {
+      component.setProps({ ...props, contact: { ...props.contact, likes: 5 } });
+      instance.increaseLikes();
+      expect(props._increaseLikes.mock.calls.length).toBe(1);
+      component.setProps({ ...props, contact: { ...props.contact, likes: 9 } });
+      instance.increaseLikes();
+      expect(props._increaseLikes.mock.calls.length).toBe(1);
+      component.setProps({ ...props, contact: { ...props.contact, likes: 23 } });
+      instance.increaseLikes();
+      expect(props._increaseLikes.mock.calls.length).toBe(1);
+    });
+  });
+  describe('decreaseLikes', () => {
+    test('if it is called', () => {
+      component.setProps({ ...props, contact: { ...props.contact, likes: 5 } });
+      instance.decreaseLikes();
+      expect(props._decreaseLikes.mock.calls.length).toBe(1);
+      component.setProps({ ...props, contact: { ...props.contact, likes: 0 } });
+      instance.decreaseLikes();
+      expect(props._decreaseLikes.mock.calls.length).toBe(1);
+      component.setProps({ ...props, contact: { ...props.contact, likes: -2 } });
+      instance.decreaseLikes();
+      expect(props._decreaseLikes.mock.calls.length).toBe(1);
+    });
+  });
+  describe('deleteContact', () => {
+    test('if it is called', () => {
+      instance.closePopUp = jest.fn();
+      instance.deleteContact();
+      expect(props._deleteContact.mock.calls.length).toBe(1);
+      expect(instance.closePopUp.mock.calls.length).toBe(1);
+    });
+  });
+  describe('openDeleteContactPopUp', () => {
+    test('if it is called', () => {
+      instance.openDeleteContactPopUp();
+      expect(instance.state.showPopup).toBeTruthy();
+      instance.setState = jest.fn();
+      instance.openDeleteContactPopUp();
+      expect(instance.setState.mock.calls.length).toBe(1);
+    });
+  });
+});
